1e7e410a635f4ca738521194346faedb21966a8f
killua <tanakasouma1130@gmail.com>
2026-06-22 19:55:13 +0900

    Fix supabase env

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl =
-  "https://wspjhbzybxzgfulsyogi.supabase.co";
-
-const supabaseKey =
-  "ここにSupabaseのPublishable keyを貼る";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(
   supabaseUrl,
